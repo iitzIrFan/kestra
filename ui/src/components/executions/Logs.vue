f7fa9c2e3f4d4600b95b71059ce58a06e01091c3
Miloš Paunović <paun992@hotmail.com>
2026-03-31 12:26:37 +0200

    fix(logs): add a key attribute to scroller items to trigger re-render when needed (#15319)
    Closes https://github.com/kestra-io/kestra-ee/issues/7203.

--- ui/src/components/executions/Logs.vue
+++ ui/src/components/executions/Logs.vue
@@ -87,6 +87,7 @@
                         :active="active"
                         :sizeDependencies="[item.message]"
                         :data-index="item.index"
+                        :key="item.uid"
                     >
                         <LogLine
                             @click="logCursor = item.index.toString()"
